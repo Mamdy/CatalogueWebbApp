fb39f6e34e7015d9c002135e5e66e51d114575e8
Mahmoud <mamdy@gmail.com>
2020-11-08 00:23:15 +0100

    pofcfile

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const app = express();
 
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/catalogue-web-app'));
+app.use(express.static('./dist/catalogue-web-app'));
 
 app.get('/*',(req, res) =>{
-	res.sendFile(path.join(__dirname +'/dist/catalogue-web-app/index.html'));
+	//res.sendFile(path.join(__dirname+'/dist/catalogue-web-app/index.html')
+	res.sendFile('index.html', { root: 'dist/catalogue-web-app' }
+	
+	);
 });
 
 // Start the app by listening on the default Heroku port
