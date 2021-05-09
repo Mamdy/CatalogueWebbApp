ccda80cecd996278f093911d6db4df657f0f489a
Mahmoud <mamdy@gmail.com>
2021-05-09 19:37:58 +0200

    userComponentConfig

--- src/app/admin-users/admin-users.component.ts
+++ src/app/admin-users/admin-users.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from '../model/User';
+import { CatalogueService } from '../services/catalogue.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-admin-users',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-users.component.css']
 })
 export class AdminUsersComponent implements OnInit {
+  @ViewChild('datatable') table: ElementRef;
+  dtOptions:any = {};
+  dataTableListeProducts$: User[]=[];
+  dtTrigger: Subject<any> = new Subject();
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+  
+  }
 
   ngOnInit() {
+    //initiliasisation de la variable options
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 50,
+      processing: true,
+      ordering: true,
+      info: true,
+      searchable: true
+    };
+  }
+
+  getAllUser(){
+
   }
 
 }
